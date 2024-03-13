df074fc8f9ebda9dc154a39e08da17365446b1cf
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2024-03-13 08:25:57 -0600

    Improved doc strings

--- cipherUtils.py
+++ cipherUtils.py
@@ -33,6 +33,12 @@ def generate_key(master_password: str, salt: str = 'salt_', iterations: int = 10
 def encrypt_password(key: bytes, password: str) -> bytes:
     """
     Encrypts the given password using the provided key.
+
+    Args:
+        key: A bytes argument used for encrypting the password.
+        password: A string password which is to be encrypted using the key.
+    Returns:
+        A bytes literal containing the encrypted password.
     """
     cipher_suite = Fernet(key)
     encrypted_password = cipher_suite.encrypt(password.encode())
@@ -41,7 +47,13 @@ def encrypt_password(key: bytes, password: str) -> bytes:
 # Decrypt a password using the generated key
 def decrypt_password(key: bytes, encrypted_password: bytes) -> str:
     """
-    Encrypts the given encrypted password using the provided key.
+    Decrypts the given encrypted password using the provided key.
+
+    Args:
+        key: A bytes argument used for decrypting the password.
+        password: A string password which is to be decrypted using the key.
+    Returns:
+        A string literal containing the decrypted, original password.
     """
     cipher_suite = Fernet(key)
     decrypted_password = cipher_suite.decrypt(encrypted_password)
@@ -51,6 +63,17 @@ def decrypt_password(key: bytes, encrypted_password: bytes) -> str:
 def embed_encrypted_password_in_image(master_password: str, password_to_encrypt: str, image_path: str, output_folder: str, salt: str = 'salt_', iterations: int = 100000) -> None:
     """
     Encrypts the provided password and embeds it in an image using steganography.
+
+    Args:
+        master_password: The master paswword used to generate the encryption key passed as a string.
+        password_to_encrypt: A string password which is to be encrypted using the key generated from the master_password, salt and iterations.
+        image_path: The path to the image that will be used to store the encrypted password.
+        output_folder:
+        salt: A string value used to add randomness to the encryption process (ideally, each salt should be random and unique).
+        iterations: The number of iterations of the underlying pseudorandom function. A higher number of iterations increases the computational cost of deriving the key.
+    
+    Returns:
+        None. The image containing the encrypted password will be created at the output_folder.
     """
     key = generate_key(master_password, salt=salt, iterations=iterations)
     encrypted_password = encrypt_password(key, password_to_encrypt)
@@ -64,7 +87,17 @@ def embed_encrypted_password_in_image(master_password: str, password_to_encrypt:
 # Decrypt the password using the main password and image
 def decrypt_password_with_image(master_password: str, encrypted_image_path: str, salt: str = 'salt_', iterations: int = 100000) -> None:
     """
-    Decrypts an encrypted password hiding in an image.
+    Encrypts the provided password and embeds it in an image using steganography.
+    
+    Args:
+        master_password: The master paswword used to generate the encryption key passed as a string.
+        encrypted_image_path: The path to the image holds the encrypted password.
+        output_folder:
+        salt: A string value used to add randomness to the encryption process (ideally, each salt should be random and unique). Must be the same used when encrypting the password in the image.
+        iterations: The number of iterations of the underlying pseudorandom function. A higher number of iterations increases the computational cost of deriving the key. Must be the same used when encrypting the password in the image.
+    
+    Returns:
+        None. The encrypted password will be extracted from the image, decrypted, and copied to the clipboard.
     """
     # Extract the encrypted password from the image using steganography
     encrypted_password = lsb.reveal(encrypted_image_path)
